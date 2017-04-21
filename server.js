9c0f59e4e1fc14cf045ba9561ca2b04841b1f9d5
nofar <nofarisraeli7@gmail.com>
2017-04-21 11:14:28 +0300

    try

--- server.js
+++ server.js
@@ -8,10 +8,6 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 })
 
-
-var handlebars = require('guarded-cliffs-30584')
-  .create({ defaultLayout:'main' });
-
 var server = app.listen(8000, function () {
    var host = server.address().address
    var port = server.address().port
